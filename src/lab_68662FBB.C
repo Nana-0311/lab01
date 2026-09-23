8802ae6e35761ece285cf5a7bd4e0b195ab5fe8b
NANA-0311 <yxiaona2025@163.com>
2026-09-23 01:19:32 -0400

    complete lab01

---
+++ src/lab_68662FBB.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ void printscreen()
+ {
+ printf(" #### \n");
+ printf("#    #\n");
+ printf("#    #\n");
+ printf("######\n");
+ printf("#    #\n");
+ printf("#    #\n");
+ printf("#    #\n");
+ }
+
+ #ifndef ___TEST___
+ int main()
+ {
+ printscreen();
+ return(0);
+ }
+ #endif
